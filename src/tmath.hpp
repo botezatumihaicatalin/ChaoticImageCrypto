8b499a1a41e65af546485769496284303ac06ab1
botezatumihaicatalin <botezatu.mihaicatalin@gmail.com>
2017-04-27 21:50:01 +0300

    Some improvements to the caching sin/cos

--- src/tmath.hpp
+++ src/tmath.hpp
@@ -8,7 +8,7 @@
 #define MASK_MAX_CIRCLE_ANGLE (MAX_CIRCLE_ANGLE - 1)
 #define T_PI 3.14159265358979323846
 
-float fast_cossin_table[MAX_CIRCLE_ANGLE];
+double fast_cossin_table[MAX_CIRCLE_ANGLE];
 
 struct table_filler {
   table_filler() {
@@ -19,19 +19,31 @@ struct table_filler {
 } table_filler_instance;
 
 inline double tcos(double n) {
-  int i = int(n * HALF_MAX_CIRCLE_ANGLE / T_PI);
+  double f = n * HALF_MAX_CIRCLE_ANGLE / T_PI;
+  int i = int(f); double d = f - i;
   if (i < 0) {
-    return fast_cossin_table[((-i) + QUARTER_MAX_CIRCLE_ANGLE) & MASK_MAX_CIRCLE_ANGLE];
+    i = ((-i) + QUARTER_MAX_CIRCLE_ANGLE) & MASK_MAX_CIRCLE_ANGLE;
   }
-  return fast_cossin_table[(i + QUARTER_MAX_CIRCLE_ANGLE) & MASK_MAX_CIRCLE_ANGLE];
+  else {
+    i = (i + QUARTER_MAX_CIRCLE_ANGLE) & MASK_MAX_CIRCLE_ANGLE;
+  }
+  double cos1 = fast_cossin_table[i];
+  double cos2 = fast_cossin_table[i + 1];
+  return cos1 + (cos2 - cos1) * d;
 }
 
 inline double tsin(double n) {
-  int i = int(n * HALF_MAX_CIRCLE_ANGLE / T_PI);
+  double f = n * HALF_MAX_CIRCLE_ANGLE / T_PI;
+  int i = int(f); double d = f - i;
   if (i < 0) {
-    return fast_cossin_table[(-((-i) & MASK_MAX_CIRCLE_ANGLE)) + MAX_CIRCLE_ANGLE];
+    i = (-((-i) & MASK_MAX_CIRCLE_ANGLE)) + MAX_CIRCLE_ANGLE;
   }
-  return fast_cossin_table[i & MASK_MAX_CIRCLE_ANGLE];
+  else {
+    i = i & MASK_MAX_CIRCLE_ANGLE;
+  }
+  double sin1 = fast_cossin_table[i];
+  double sin2 = fast_cossin_table[i + 1];
+  return sin1 + (sin2 - sin1) * d;
 }
 
 inline double ttan(double n) {
